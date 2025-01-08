d4c46e7379343e1084ba8219951ed499ecc9df7b
legendtosif <toofanxd72@gmail.com>
2025-01-09 01:29:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
-API_ID = 
+API_ID = "276936e1a27704b61a9ceaf6d379adf5"
 
-API_HASH = ""
+API_HASH = "26841295"
 
 # Telegram bot token
-TELEGRAM_TOKEN = ""
+TELEGRAM_TOKEN = "7807373262:AAEGtxIYlbNQeS-aJu0yyMgE3zcjqrkizik"
 
 # Your telegram user ID (owner)
-OWNER_ID = 6955568347
+OWNER_ID = 7520092354
 
-SUDO_ID = [1679112664]
+SUDO_ID = [6519786491]
 
-MONGO_URI = ""
+MONGO_URI = "mongodb+srv://Jiosaavan:Jiosaavan@cluster0.lv9br.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-DB_NAME = "krisheditGuadian"
+DB_NAME = "Jiosaavan"
 
 LOGGER = True
 
